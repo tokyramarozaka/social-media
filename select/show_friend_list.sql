8f8aa703bf84cf8f6b080c94668bfb022be64025
Tokimahery <101268219+tokyramarozaka@users.noreply.github.com>
2022-07-13 09:41:32 +0300

    Create show_friend_list.sql
    Added a request to show all the friends of a certain account whose id is 1. Union is used to build the result, as friendship is commutative : 1 is a friend of 2, means automatically that 2 is a friend of 1.

---
+++ select/show_friend_list.sql
@@ -0,0 +1,28 @@
+-- Creates a view fetching all the friends of account id = 1
+
+-- Performs an union with all the accepted requests from 1
+SELECT receiver.first_name,
+    receiver.last_name,
+    receiver.nickname,
+    receiver.profile_pic, 
+    friendship_birthday
+FROM 
+    send_invite
+INNER JOIN account AS receiver
+    ON receiver.id_account = send_invite.id_account_receiver 
+WHERE 
+    send_invite.id_account_sender = 1
+    AND is_accepted = true
+UNION -- and the accepted requests to 1
+    SELECT sender.first_name,
+        sender.last_name,
+        sender.nickname,
+        sender.profile_pic, 
+        friendship_birthday
+    FROM 
+        send_invite
+    INNER JOIN account AS sender
+        ON sender.id_account = send_invite.id_account_sender 
+    WHERE 
+        send_invite.id_account_receiver = 1
+        AND is_accepted = true;
